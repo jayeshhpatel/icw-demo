a81bbd287352b6c7742bc9221f2720cd67c68be1
Jayesh Patel <send2jayesh@gmail.com>
2021-05-03 11:12:14 +0530

    Update Page (PC)

--- html-amananet/html/assets/js/main.js
+++ html-amananet/html/assets/js/main.js
@@ -1,20 +1,24 @@
 $(document).ready(function() { 
-    if($('.main-header').length) {  
-        var header = $(".sub-header");
-        $(window).scroll(function() {
-            var scroll = $(window).scrollTop();
-            if (scroll >= 5) {
-                header.addClass("is-fixed");
-            } else {
-                header.removeClass('is-fixed');
-            }
-        });
-        $(".toggle-sidebar").click(function() {
-            $(".sub-header").toggleClass("mobile-menu");
-            $(".sub-header .collapse-nav").toggleClass("open");
-            $('body').toggleClass('overflow-hidden');
-            $(this).toggleClass('toggle');
-        })
+   /* Fixed Header - light header */
+	if ($('.main-header').length) {
+        var window_height = $( window ).height();
+        var main_height = $('.main-header').outerHeight();
+        var document_height = $( document ).height();       
+        if(document_height > (window_height+main_height)) {
+            var stickyHeaderTop = $('.main-header .sub-header').offset().top;
+            $(window).scroll(function() {
+                console.log(window_height);
+                if($(this).scrollTop() > stickyHeaderTop){
+                    $("body").addClass("is--fixed");
+                    $(".main-header .sub-header").addClass("is-fixed");
+                }
+                else if($(this).scrollTop() <= stickyHeaderTop){
+                    $("body").removeClass("is--fixed");
+                    $(".main-header .sub-header").removeClass("is-fixed");
+                }
+            });
+            $(window).scroll();
+        }
     }
     var swiper = new Swiper(".banner-slider", {
         pagination: {
