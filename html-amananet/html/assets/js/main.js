002fa65dbb1f988e582bc243be454bcb61fc6249
Jayesh Patel <send2jayesh@gmail.com>
2021-05-04 19:45:34 +0530

    Update CSS (PC)

--- html-amananet/html/assets/js/main.js
+++ html-amananet/html/assets/js/main.js
@@ -1,25 +1,26 @@
 $(document).ready(function() { 
    /* Fixed Header - light header */
-	if ($('.main-header').length) {
-        var window_height = $( window ).height();
-        var main_height = $('.main-header').outerHeight();
-        var document_height = $( document ).height();       
-        if(document_height > (window_height+main_height)) {
-            var stickyHeaderTop = $('.main-header .sub-header').offset().top;
-            $(window).scroll(function() {
-                console.log(window_height);
-                if($(this).scrollTop() > stickyHeaderTop){
-                    $("body").addClass("is--fixed");
-                    $(".main-header .sub-header").addClass("is-fixed");
-                }
-                else if($(this).scrollTop() <= stickyHeaderTop){
-                    $("body").removeClass("is--fixed");
-                    $(".main-header .sub-header").removeClass("is-fixed");
-                }
-            });
-            $(window).scroll();
-        }
-    }
+	// if ($('.main-header').length) {
+    //     var window_height = $( window ).height();     
+    //     var stickyHeaderTop = $('.main-header .sub-header').offset().top;
+    //     $(window).scroll(function() {
+    //         console.log(window_height);
+    //         if($(this).scrollTop() >= stickyHeaderTop){
+    //             $(".main-header .sub-header").addClass("is-fixed");
+    //         }
+    //         else if($(this).scrollTop() <= stickyHeaderTop){
+    //             $(".main-header .sub-header").removeClass("is-fixed");
+    //         }
+    //     });
+    //     $(window).scroll();
+    // }
+    $(".toggle-sidebar").click(function() {
+        $(".sub-header").toggleClass("mobile-menu");
+        $(".top-header").toggleClass("d-none");
+        $(".sub-header .collapse-nav").toggleClass("open");
+        $('body').toggleClass('overflow-hidden');
+        $(this).toggleClass('toggle');
+    })
     var swiper = new Swiper(".banner-slider", {
         pagination: {
             el: ".swiper-pagination",
@@ -30,3 +31,47 @@ $(document).ready(function() {
         },
     });
 });
+
+// Hide header on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.main-header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+    if($(this).scrollTop() >= 5){ 
+        $('.main-header').addClass('sticky-header');
+    } else {
+        $('.main-header').removeClass('sticky-header');
+    }
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If scrolled down and past the navbar, add class .nav-up.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.main-header').removeClass('nav-down').addClass('nav-up');
+        
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.main-header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+  
+    lastScrollTop = st;
+}
